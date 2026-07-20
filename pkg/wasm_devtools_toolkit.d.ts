639e8ae2702791dd112a357abed0d5cd6705c840
gijile <salakgijil@gmail.com>
2026-07-20 02:58:48 +0000

    Remove pkg ignore rule and add pkg artifacts

---
+++ pkg/wasm_devtools_toolkit.d.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export class JsonFormatter {
+    free(): void;
+    [Symbol.dispose](): void;
+    is_done(): boolean;
+    constructor(json: string);
+    process_chunk(chunk_size: number): string;
+}
+
+export function match_selector(selector: string, element: Element): boolean;
+
+export function parse_css(css: string): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_jsonformatter_free: (a: number, b: number) => void;
+    readonly jsonformatter_is_done: (a: number) => number;
+    readonly jsonformatter_new: (a: number, b: number) => number;
+    readonly jsonformatter_process_chunk: (a: number, b: number, c: number) => void;
+    readonly match_selector: (a: number, b: number, c: number, d: number) => void;
+    readonly parse_css: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_export: (a: number, b: number) => number;
+    readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_export3: (a: number) => void;
+    readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+    readonly __wbindgen_export4: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
